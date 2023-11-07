abedf74f20cf4cb269a26536fb31b62d46534d23
muaz-dev <79562872+muaz-dev@users.noreply.github.com>
2023-11-07 19:45:58 +0700

    11/7/23

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/apologies') // Corrected the path here
+  getApologies(): string {
+    return this.appService.getApologies(); // You might want to call a different method in AppService that returns apologies
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! Muaz Hayee';
+  }
+  getApologies(): string {
+    return 'I would like to do apologies';
   }
 }
